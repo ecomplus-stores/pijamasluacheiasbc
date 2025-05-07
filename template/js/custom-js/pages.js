5a90d01e4af7918995e8275a2c5dc78c1a14afe3
Vitor Rocha Gonçalves <35348281+vitorrgg@users.noreply.github.com>
2025-05-07 19:10:05 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,21 @@
 // Add your custom JavaScript for storefront pages here.
+import EcomSearch from '@ecomplus/search-engine'
+
+EcomSearch.dslMiddlewares.push((dsl) => {
+  if (dsl.query && dsl.query.bool) {
+    if (dsl.query.bool.filter) {
+      dsl.query.bool.filter.forEach(fixCategoryIdsFilter)
+    }
+    if (dsl.query.bool.must) {
+      dsl.query.bool.must.forEach((filter) => {
+        if (filter.multi_match) {
+          const { fields } = filter.multi_match
+          if (Array.isArray(fields)) {
+            fields.push('skus')
+          }
+        }
+        fixCategoryIdsFilter(filter)
+      })
+    }
+  }
+})
