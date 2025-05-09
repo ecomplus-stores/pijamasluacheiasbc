d128607d3116ca420b5c01e31180aa7dc5cd3336
Vitor Rocha Gonçalves <35348281+vitorrgg@users.noreply.github.com>
2025-05-09 14:09:24 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,6 +1,17 @@
 // Add your custom JavaScript for storefront pages here.
 import EcomSearch from '@ecomplus/search-engine'
 
+const fixCategoryIdsFilter = ({ terms }) => {
+  if (
+    terms &&
+    terms['categories.name'] &&
+    /^[0-9a-f]{24}$/.test(terms['categories.name'][0])
+  ) {
+    terms['categories._id'] = terms['categories.name']
+    delete terms['categories.name']
+  }
+}
+
 EcomSearch.dslMiddlewares.push((dsl) => {
   if (dsl.query && dsl.query.bool) {
     if (dsl.query.bool.filter) {
